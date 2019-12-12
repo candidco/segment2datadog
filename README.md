e47216d160d7ba6125bca9e2a2f9e1dab504eaaa
Vageli Mouzakitis <vagelim@users.noreply.github.com>
2019-12-12 15:50:45 -0500

    Remove direct API calls (#2)
    * set logging to stdout
    
    * remove use of api directly
    we can put a dd container sidecar and avoid the blocking http request, offloading the work of metric submission to the agent. also removes the need for this code to know dd secrets
    
    * apply formatting
    
    * add formatting check
    
    * update readme

--- README.md
+++ README.md
@@ -17,17 +17,12 @@ The flask app will forward the received events to Datadog as a counter.
 ## Installation:
 
 - Set the corresponding keys (you can access your Datadog keys [here](https://app.datadoghq.com/account/settings#api)):
-  - **DD_API_KEY**: Your Datadog API key.
-  - **DD_APP_KEY**: Your Datadog app key.
-  - **DD_HOSTNAME**: Add this key to the agent, pointing to the URL of the flask app.
+  - **DATADOG_STATSD_HOST**: IP address of a [datadog agent](https://app.datadoghq.com/account/settings#agent) (be sure you've set `DD_DOGSTATSD_NON_LOCAL_TRAFFIC` in the agent if you must).
   - **SEGMENT_SHARED_SECRET**: Your Segment Webhooks destination Shared Secret.
 - Add [Webhooks](https://segment.com/docs/destinations/webhooks/) destination in your Segment project.
-- Configure your Webhooks destination by setting the webhook URL to your Heroku
-app, eg: https://&lt;APP_NAME&gt;.herokuapp.com/api/&lt;SOURCE&gt;, where APP_NAME is
-your Heroku app name and SOURCE should be the name of the Segment source you are
-retrieving events from.
+- Configure your Webhooks destination by setting the webhook URL to a CNAME pointing to the lb fronting the service.
 - Activate the webhook destination.
 - You should start receiving events in your Datadog dashboard, named **segment.event**.
 Datadog tags are included like **source**, **type** and **event** (segment source, type of event (track)
 and event name, respectively). You can customize app.py file to add your own
-tags or also monitor other type of events, eg: page events.
+tags or also monitor [other type of events](https://segment.com/docs/connections/spec/), eg: [page events](https://segment.com/docs/connections/spec/page/).
--- app.py
+++ app.py
@@ -2,38 +2,44 @@ import hmac
 import hashlib
 import os
 from flask import Flask, jsonify, request, abort
-from datadog import initialize, api, statsd
+from datadog import initialize, statsd
 
 # get keys from enfironment variables
-SEGMENT_SHARED_SECRET = os.environ['SEGMENT_SHARED_SECRET']
-DATADOG_API_KEY = os.environ['DD_API_KEY']
-DATADOG_APP_KEY = os.environ['DD_APP_KEY']
+SEGMENT_SHARED_SECRET = os.environ["SEGMENT_SHARED_SECRET"]
 
 # initialize datadog
-options = {
-    'api_key': DATADOG_API_KEY,
-    'app_key': DATADOG_APP_KEY
-}
+options = {"statsd_host": os.environ.get("DATADOG_STATSD_HOST", "127.0.0.1")}
 
 initialize(**options)
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Segment2Datadog is up and running!"
 
-@app.route('/api/<string:source>', methods=['POST'])
+
+@app.route("/api/<string:source>", methods=["POST"])
 def segment2datadog(source):
     # check signature
-    signature = request.headers['x-signature']
-    digest = hmac.new(SEGMENT_SHARED_SECRET.encode(), msg=request.data, digestmod=hashlib.sha1).hexdigest()
+    signature = request.headers["x-signature"]
+    digest = hmac.new(
+        SEGMENT_SHARED_SECRET.encode(), msg=request.data, digestmod=hashlib.sha1
+    ).hexdigest()
     if digest != signature:
-        abort(403, 'Signature not valid.')
+        abort(403, "Signature not valid.")
     if not source:
-        abort(404, 'Source parameter not present.')
+        abort(404, "Source parameter not present.")
     content = request.get_json(silent=True)
     # increment event counter in datadog
-    if content['type'] == 'track':
-        statsd.increment('segment.event', tags = ['source:' + source, 'event:' + '-'.join(content['event'].split()), 'type:' + content['type']])
-    return jsonify({'source': source, 'data': content})
+    if content["type"] == "track":
+        statsd.increment(
+            "segment.event",
+            tags=[
+                "source:" + source,
+                "event:" + "-".join(content["event"].split()),
+                "type:" + content["type"],
+            ],
+        )
+    return jsonify({"source": source, "data": content})
